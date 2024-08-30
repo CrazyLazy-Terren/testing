4e6ec9cf9a242f08af7690bfcdafc3ab297fa58b
CrazyLazy <crazylazy007@gmail.com>
2024-08-30 18:48:28 +0800

    Update dependence

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -186,7 +186,7 @@ export default function DataTable<TData, TValue>({ columns, data, moveRow, moveA
                       {headerGroup.headers.map((header) => {
                         return (
                           <TableHead key={header.id} header={header}>
-                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext()) || null}
                           </TableHead>
                         )
                       })}
